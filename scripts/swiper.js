0c6a4f17423214c9d32c26d24fe8a443b9775f67
farxod <farxodyufff@gmail.com>
2023-04-28 06:19:15 +0300

    added animation

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -36,3 +36,63 @@ menu.addEventListener('click', function (event){
         list.classList.remove('active')
     }
 })
+
+
+gsap.from('.header__info__paragraph', {
+    y: 100,
+    opacity: 0,
+    duration: 1
+})
+gsap.from('.header__info__description', {
+    y: 100,
+    opacity: 0,
+    duration: 1
+})
+
+let proImg = document.querySelectorAll('.products__img')
+let proCard = document.querySelectorAll('.products__card');
+let solCard = document.querySelectorAll('.solution__card');
+let about = document.querySelectorAll('.about');
+proImg.forEach(el =>{
+    gsap.from(el, {
+        opacity:0,
+        y:50,
+        duration:1,
+        scrollTrigger:{
+            trigger: el,
+        }
+    })
+})
+proCard.forEach(el =>{
+    gsap.to(el, {
+        y: -200,
+        scrollTrigger:{
+            trigger: el,
+            scrub: 1
+        }
+    })
+})
+solCard.forEach(el =>{
+    gsap.to(el, {
+        y: -150,
+        scrollTrigger:{
+            trigger: el,
+            scrub: 1
+        }
+    })
+})
+about.forEach(el =>{
+    gsap.from(el, {
+        y:100,
+        opacity: 0,
+        duration:1,
+        scrollTrigger:{
+            trigger: el,
+        }
+    })
+})
+gsap.from('.about__info__header', {
+    y:100,
+    opacity: 0,
+    duration:1,
+})
